b1677bd75a93101ab90ec591d7b81b0ae32b2146
PixelPuncher6638 <christophervistian@gmail.com>
2022-03-26 04:30:38 -0500

    UI working now

---
+++ POINT-VR-Chapter-1/Assets/UIAssets/FollowTransform.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTransform : MonoBehaviour
+{
+    [SerializeField] private Transform lookAt;
+    [SerializeField] private Transform transformToFollow;
+    [SerializeField] private float followSpeed;
+    private Transform _thisTransform;
+    void Start()
+    {
+        _thisTransform = transform;
+    }
+
+
+    void Update()
+    {
+        _thisTransform.LookAt(lookAt, Vector3.up);
+        // _thisTransform.Rotate(0f, 180f, 0f);
+        var newPosition = _thisTransform.position;
+        var followPosition = transformToFollow.position;
+        newPosition.x = Mathf.Lerp(newPosition.x, followPosition.x, followSpeed * Time.deltaTime);
+        newPosition.y = Mathf.Lerp(newPosition.y, followPosition.y, followSpeed * Time.deltaTime);
+        newPosition.z = Mathf.Lerp(newPosition.z, followPosition.z, followSpeed * Time.deltaTime);
+    }
+}
