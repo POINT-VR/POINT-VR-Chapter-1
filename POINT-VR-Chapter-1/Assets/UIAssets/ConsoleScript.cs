0d6d200f61060dbfcdc9b376f23b0269ba826a66
PixelPuncher6638 <christophervistian@gmail.com>
2022-04-25 17:41:18 -0500

    Left Menu Button + Console + New Build APK

---
+++ POINT-VR-Chapter-1/Assets/UIAssets/ConsoleScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ConsoleScript : MonoBehaviour
+{
+    public string startingText;
+    [SerializeField]
+    public Text objectToMutate;
+    void Start()
+    {
+        objectToMutate.text = startingText + '\n';
+    }
+    public void Log(string s)
+    {
+        objectToMutate.text = objectToMutate.text.Clone() + s + '\n';
+    }
+}
