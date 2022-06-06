0b522b73d9dcd38d2d370d15783d2aedc73012b3
sonalij3 <sonalij3@illinois.edu>
2022-06-06 15:09:20 -0500

    Print out text to the console
    Temporary string can be printed out to the console. Working on getting controller input to the console next.

--- POINT-VR-Chapter-1/Assets/UIAssets/ConsoleScript.cs
+++ POINT-VR-Chapter-1/Assets/UIAssets/ConsoleScript.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 public class ConsoleScript : MonoBehaviour
 {
     public string startingText;
+    public string updatingText;
     [SerializeField]
     public Text objectToMutate;
     void Start()
@@ -15,8 +16,37 @@ public class ConsoleScript : MonoBehaviour
     /// <summary>
     /// Public function that can append a string to the console.
     /// </summary>
+
     public void Log(string s)
     {
         objectToMutate.text = objectToMutate.text.Clone() + s + '\n';
+    }
+
+    /// Appends the current controller input to the string s
+    public void ControlInput()
+    {
+        string s;
+
+        // Temp to see if it prints out to screen and how often
+        s = "Hi";
+
+        // Avoids multiple same texts from being printed to the screen
+        if (updatingText == s)
+            return;
+        else
+            updatingText = s;
+
+        // Get Controller Input - TODO
+
+        // Convert to readable string - TODO
+
+        // Output to Log
+        Log(updatingText);
+    }
+
+    void Update()
+    {
+        // Function prints out Controller Inputs to console
+        ControlInput();
     }
 }
