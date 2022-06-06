0fa7c54e21fbca4f6fc9938087d2e022671ab64e
sonalij3 <sonalij3@illinois.edu>
2022-06-06 15:37:29 -0500

    Cleaned up

--- POINT-VR-Chapter-1/Assets/UIAssets/ConsoleScript.cs
+++ POINT-VR-Chapter-1/Assets/UIAssets/ConsoleScript.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit; // Don't know if we need this package
+using UnityEngine.InputSystem;
 
 public class ConsoleScript : MonoBehaviour
 {
@@ -9,6 +11,7 @@ public class ConsoleScript : MonoBehaviour
     public string updatingText;
     [SerializeField]
     public Text objectToMutate;
+
     void Start()
     {
         objectToMutate.text = startingText + '\n';
@@ -20,25 +23,31 @@ public class ConsoleScript : MonoBehaviour
     public void Log(string s)
     {
         objectToMutate.text = objectToMutate.text.Clone() + s + '\n';
+        // TODO: Need a way to clear the console text. If there is too much text it overfills
     }
 
     /// Appends the current controller input to the string s
     public void ControlInput()
     {
+        var gamepad = Gamepad.current;
         string s;
 
         // Temp to see if it prints out to screen and how often
         s = "Hi";
 
-        // Avoids multiple same texts from being printed to the screen
+        /// Get Controller Input - TODO
+
+        // Breaks the system
+        //if (gamepad.rightTrigger.wasPressedThisFrame)
+        //    s = "rightTrigger";
+
+        /// Convert to readable string - TODO
+
+        /// Avoids multiple same texts from being printed to the screen
         if (updatingText == s)
             return;
         else
             updatingText = s;
-
-        // Get Controller Input - TODO
-
-        // Convert to readable string - TODO
 
         // Output to Log
         Log(updatingText);
