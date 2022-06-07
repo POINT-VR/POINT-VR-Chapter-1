f93471a684d79ec2c496abdbb701c771923d398a
PixelPuncher6638 <christophervistian@gmail.com>
2022-06-06 19:44:16 -0500

    Dual hand controllers + build

---
+++ POINT-VR-Chapter-1/Assets/XR/TeleportController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.Events;
+
+public class TeleportController : MonoBehaviour
+{
+    public GameObject controller;
+    public GameObject teleporter;
+    public InputActionReference teleportationReference;
+    public UnityEvent teleportActivate;
+    public UnityEvent teleportCancel;
+    private void Start()
+    {
+        teleportationReference.action.performed += TeleportModeActivate;
+        teleportationReference.action.canceled += TeleportModeDeactivate;
+    }
+    private void TeleportModeActivate(InputAction.CallbackContext ctx) => teleportActivate.Invoke();
+    private void TeleportModeDeactivate(InputAction.CallbackContext ctx) => Invoke(nameof(Cancel), .1f);
+    private void Cancel() => teleportCancel.Invoke();
+}
