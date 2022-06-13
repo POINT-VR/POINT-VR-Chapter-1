1687f2d9e78aff226889907a7813f34f53f2915d
PixelPuncher6638 <christophervistian@gmail.com>
2022-06-13 00:01:21 -0500

    again

--- POINT-VR-Chapter-1/Assets/XR/TeleportController.cs
+++ POINT-VR-Chapter-1/Assets/XR/TeleportController.cs
@@ -6,15 +6,23 @@ public class TeleportController : MonoBehaviour
 {
     public GameObject controller;
     public GameObject teleporter;
-    public InputActionReference teleportationReference;
+    public InputActionReference teleportationReference, cancellationReference;
     public UnityEvent teleportActivate;
     public UnityEvent teleportCancel;
-    private void Start()
+    private void OnEnable()
     {
         teleportationReference.action.performed += TeleportModeActivate;
         teleportationReference.action.canceled += TeleportModeDeactivate;
+        cancellationReference.action.canceled += CancelNow;
+    }
+    private void OnDisable()
+    {
+        teleportationReference.action.performed -= TeleportModeActivate;
+        teleportationReference.action.canceled -= TeleportModeDeactivate;
+        cancellationReference.action.canceled -= CancelNow;
     }
     private void TeleportModeActivate(InputAction.CallbackContext ctx) => teleportActivate.Invoke();
-    private void TeleportModeDeactivate(InputAction.CallbackContext ctx) => Invoke("Cancel", .1f);
+    private void TeleportModeDeactivate(InputAction.CallbackContext ctx) => Invoke(nameof(Cancel), .1f);
+    private void CancelNow(InputAction.CallbackContext ctx) => teleportCancel.Invoke();
     void Cancel() => teleportCancel.Invoke();
 }
