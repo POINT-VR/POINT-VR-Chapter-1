16d2d1a4c6be7948407244fca9c2e68450d9399b
sonalij3 <sonalij3@illinois.edu>
2022-11-04 12:49:00 -0500

    Split MeshDeformScript into two scripts
    One is optimized for 8 vertices at the center of each grid point. The other one is a generic script that should work on all grid types.

--- POINT-VR-Chapter-1/Assets/POINT/Grid/OptimalMeshDeformScript.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Grid/OptimalMeshDeformScript.cs
@@ -1,9 +1,10 @@
 ﻿using UnityEngine;
 [RequireComponent(typeof(MeshRenderer))]
-public class MeshDeformScript : MonoBehaviour
+public class OptimalMeshDeformScript : MonoBehaviour
 {
     // <summary>
-    // Generic MeshDeformScipt. Should work on any Grid Model.
+    // Optimized MeshDeformScipt for specific grid models.
+    // Works on grid models that have not been touched in Blender and grid models that have been optimized for reduced number of verticies.
     // </summary>
 
     // <summary>
@@ -27,22 +28,32 @@ public class MeshDeformScript : MonoBehaviour
     {
         deformingMesh = GetComponent<MeshFilter>().mesh;
         displacedVertices = (Vector3[]) deformingMesh.vertices.Clone();
-        originalVertices  = (Vector3[]) deformingMesh.vertices.Clone();
+        originalVertices = new Vector3[displacedVertices.Length/8];
+        for (int i = 0; i < originalVertices.Length; i++)
+        {
+            originalVertices[i] = displacedVertices[8*i + 1];
+        }
     }
 
     private void FixedUpdate()
     {
-        for (int i = 0; i < displacedVertices.Length; i ++)
+        for (int i = 1; i < displacedVertices.Length; i += 8)
         {
-            Vector3 currentPosition = originalVertices[i];
+            Vector3 currentPosition = originalVertices[(i-1)/8];
             for (int j = 0; j < rigidbodiesToDeformAround.Length; j++)
             {
                 Vector3 direction = currentPosition - rigidbodiesToDeformAround[j].transform.position;
                 float distance = (power * rigidbodiesToDeformAround[j].mass * direction.sqrMagnitude) / (1f + (direction.sqrMagnitude) * (direction.sqrMagnitude));
                 currentPosition -= distance * direction;
             }
+            displacedVertices[i - 1] = currentPosition - new Vector3(thickness, 0f, 0f);
             displacedVertices[i] = currentPosition;
-
+            displacedVertices[i + 1] = currentPosition - new Vector3(thickness, thickness, 0f);
+            displacedVertices[i + 2] = currentPosition - new Vector3(0f, thickness, 0f);
+            displacedVertices[i + 3] = currentPosition - new Vector3(thickness, 0f, thickness);
+            displacedVertices[i + 4] = currentPosition - new Vector3(0f, 0f, thickness);
+            displacedVertices[i + 5] = currentPosition - new Vector3(thickness, thickness, thickness);
+            displacedVertices[i + 6] = currentPosition - new Vector3(0f, thickness, thickness);
             // cutoff code is archived here for now. note that the cutoff variable has been commented out as well.
             //           if (direction.sqrMagnitude < cutoff)
             //           {
