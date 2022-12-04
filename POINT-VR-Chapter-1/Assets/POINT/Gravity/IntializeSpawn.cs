7804f24e7a4a027ee3f9c13718fee0ed17d2d038
sonalij3 <sonalij3@illinois.edu>
2022-12-04 13:39:27 -0600

    Initial mass objects spawn points for each scene

---
+++ POINT-VR-Chapter-1/Assets/POINT/Gravity/IntializeSpawn.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(MeshRenderer))]
+public class IntializeSpawn : MonoBehaviour
+{
+    /// <summary>
+    /// Generic GravityScipt. Should work on any Mass Objects with a Collider.
+    /// </summary>
+    
+    /// <summary>
+    /// Mass object that the script is attached to
+    /// </summary>
+    public Rigidbody massObject;
+    public Vector3 intialPos;
+    //[Header("Other Constants")]
+
+    void Start()
+    {
+        massObject = GetComponent<Rigidbody>(); // This is the massobject that will experience gravity
+    }
+
+    void Intialize()
+    {
+        massObject.transform.position = intialPos;
+        massObject.velocity = Vector3.zero;
+    }
+
+    private void FixedUpdate()
+    {
+        
+    }
+}
