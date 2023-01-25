49bd282bc66ec9a596c37a567f2c4f3bb99c986f
sonalij3 <sonalij3@illinois.edu>
2023-01-25 01:39:12 -0600

    Commented IntializeSpawn
    Also attached the Cast() commands on the SceneUI elements for mass sphere 2

--- POINT-VR-Chapter-1/Assets/POINT/Gravity/IntializeSpawn.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Gravity/IntializeSpawn.cs
@@ -3,13 +3,19 @@
 public class IntializeSpawn : MonoBehaviour
 {
     /// <summary>
-    /// Generic GravityScipt. Should work on any Mass Objects with a Collider.
+    /// Generic IntializeSpawn. Should work on any Mass Objects with a RigidBody.
+    /// When sent a message "Intialize" will rest a Mass Object to some intial position.
+    /// Meant to be used in combination with the UI collider script. 
     /// </summary>
     
     /// <summary>
     /// Mass object that the script is attached to
     /// </summary>
     public Rigidbody massObject;
+
+    /// <summary>
+    /// The initialPos the Mass Object will be reset to.
+    /// </summary>
     public Vector3 intialPos;
     //[Header("Other Constants")]
 
