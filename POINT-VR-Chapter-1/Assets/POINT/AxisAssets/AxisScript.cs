53e37fe68d650deba0fa4d83c050439fab01d917
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-01-29 19:43:47 -0600

    Added 3D-marker object
    Scene, axis obj files, axis materials, and axis script(works on game runtime)

---
+++ POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.CoreModule.GameObject;
+
+public class AxisScript : MonoBehaviour
+{
+    public GameObject XAxis;
+    public GameObject YAxis;
+    public GameObject ZAxis;
+    private GameObject XBody;
+    private GameObject YBody;
+    private GameObject ZBody;
+    public Vector3 scale = new Vector3(1,1,1);
+    //TODO: Fine-tune moveent of axes wrt scale
+    public void scaleAxes(float x_scale, float y_scale, float z_scale)
+    {
+ 
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //XAxis = GameObject.Find("XAxis").GetComponent<Transform>();
+        //YAxis = GameObject.Find("YAxis").GetComponent<Transform>();
+       //ZAxis = GameObject.Find("ZAxis").GetComponent<Transform>();
+        XBody = GameObject.Find("XBody");
+        YBody = GameObject.Find("YBody");
+        ZBody = GameObject.Find("ZBody");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        XBody.transform.localScale = new Vector3(1,scale.x,1);
+        YBody.transform.localScale = new Vector3(1, scale.y, 1);
+        ZBody.transform.localScale = new Vector3(1, scale.z, 1);
+        XAxis.transform.localPosition = new Vector3(scale.x/4 - (1 / 4), ((float)0.146)+0, 0);
+        YAxis.transform.localPosition = new Vector3(0, ((float)0.159)+scale.y/4-(1/4), 0);
+        ZAxis.transform.localPosition = new Vector3(0,((float)0.146) + 0, scale.z/4 - (1 / 4));
+        //XBody.transform.localScale = scale;
+        //YBody.transform.localScale = scale;
+        //ZBody.transform.localScale = scale;
+    }
+}
