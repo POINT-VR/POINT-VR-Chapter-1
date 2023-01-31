0b8667c1a10706316761b2eebf7e6cfac3286069
sonalij3 <sonalij3@illinois.edu>
2023-01-30 23:34:20 -0600

    Attachable Emulator components
    Created the Emulator in an ignorable folder

---
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/EditorTesting/EditorHandController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(XRHardwareController))]
+public class EditorHandController : MonoBehaviour
+{
+    /// <summary>
+    /// The amount by which this hand moves per frame of action
+    /// </summary>
+    [SerializeField] float delta;
+    /// <summary>
+    /// Input reference for the arrow keys
+    /// </summary>
+    [SerializeField] InputActionReference motionReference;
+#if UNITY_EDITOR
+    private void OnEnable()
+    {
+        GetComponent<XRHardwareController>().enabled = false; //no conflict of movement between this and XRHardwareController
+        motionReference.action.Enable();
+        transform.parent.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = false; //ensure the left hand stays above the right hand in the hierarchy
+    }
+    private void OnDisable()
+    {
+        GetComponent<XRHardwareController>().enabled = true;
+        motionReference.action.Disable();
+        transform.parent.gameObject.GetComponentInChildren<SkinnedMeshRenderer>().enabled = true;
+    }
+    private void Update()
+    {
+        Vector2 pos = motionReference.action.ReadValue<Vector2>();
+        transform.localPosition = new Vector3(pos.x * delta + transform.localPosition.x, pos.y * delta + transform.localPosition.y, 0f);
+    }
+#endif
+}
---
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/EditorTesting/EditorPlayerRotator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+public class EditorPlayerRotator : MonoBehaviour
+{
+    [SerializeField] InputActionReference editorPlayerRotatationOverride;
+    [SerializeField] float sensY;
+    [SerializeField] float sensX;
+#if UNITY_EDITOR //This test script is only for the editor
+    private void OnEnable()
+    {
+        editorPlayerRotatationOverride.action.Enable();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    private void OnDisable()
+    {
+        editorPlayerRotatationOverride.action.Disable();
+        Cursor.lockState = CursorLockMode.None;
+    }
+    void LateUpdate() //One thing to keep in mind: this does rotate the whole playerbase. While not elegant, it is simple for editor testing.
+    {
+        Vector2 angle = editorPlayerRotatationOverride.action.ReadValue<Vector2>().normalized;
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x - angle.y * sensY, transform.rotation.eulerAngles.y + angle.x * sensX, 0f);
+    }
+#endif
+}
