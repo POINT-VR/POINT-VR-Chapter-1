9574b51fedf81eca6363f4ab6ecf06b8538ff451
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-03-04 23:05:36 -0600

    Modified AxisMaster script to work with the UI collider

--- POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisMaster.cs
+++ POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisMaster.cs
@@ -8,7 +8,7 @@ public class AxisMaster : MonoBehaviour
     public Vector3 scale = new Vector3(1,1,1);
     private AxisScript axisScript;
     private Vector3 scaleInitial;
-    private Vector3 scaleFinal;
+    public Vector3 scaleFinal = new Vector3(4.0f, 2.0f, 9.0f);
     private float t = 0.0f;
 
     public float speed = 0.05f;
@@ -21,14 +21,14 @@ public class AxisMaster : MonoBehaviour
         
     }
 
-    public void scaleAxes(Vector3 scale)
-    {
-        scaleFinal = scale;
-        t = 0.0f;
-    }
+   // public void scaleAxes(Vector3 scale)
+   // {
+    //    scaleFinal = scale;
+    //    t = 0.0f;
+   // }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         if (t <= 1.0f)
         {
