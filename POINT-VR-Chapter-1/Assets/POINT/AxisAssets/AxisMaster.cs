968f3da10f81b32e78e7f4f6a09af45af8fdfefa
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-03-27 00:39:24 -0500

    Documentation of both scripts

--- POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisMaster.cs
+++ POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisMaster.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class AxisMaster : MonoBehaviour
 {
+    /**
+     * Connect AxisMaster to the 3Daxis component
+     * THIS SCRIPT CONTROLS THE MOVEMENT OF THE AXES
+     * Change scaleFinal to the desired values (Vector3, not below 0), and use method scaleAxes to change the axes scale
+    **/
+
 
     public Vector3 scale = new Vector3(1,1,1);
     private AxisScript axisScript;
@@ -16,6 +22,7 @@ public class AxisMaster : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Initializes values
         axisScript = GetComponent<AxisScript>();
         scaleInitial = axisScript.scale;
         
@@ -23,6 +30,7 @@ public class AxisMaster : MonoBehaviour
 
     public void scaleAxes()
    {
+    //resets time constant, setting the loop up again
     //    scaleFinal = scale;
         t = 0.0f;
    }
@@ -30,6 +38,7 @@ public class AxisMaster : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Loop lerps scale from initial value from AxisScript to scaleFinal
         if (t <= 1.0f)
         {
             axisScript.scale = new Vector3(Mathf.Lerp(scaleInitial.x, scaleFinal.x, t / duration), Mathf.Lerp(scaleInitial.y, scaleFinal.y, t / duration),
--- POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisScript.cs
+++ POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisScript.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 
 public class AxisScript : MonoBehaviour
 {
+    /**
+     * AxisScript scales the individual axes for a specific scale value at one point in time
+     * **/
     public GameObject XAxis;
     public GameObject YAxis;
     public GameObject ZAxis;
@@ -35,6 +38,8 @@ public class AxisScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Sets up the local GameObject references and scale variables
+
         //XAxis = GameObject.Find("XAxis").GetComponent<Transform>();
         //YAxis = GameObject.Find("YAxis").GetComponent<Transform>();
        //ZAxis = GameObject.Find("ZAxis").GetComponent<Transform>();
@@ -53,12 +58,13 @@ public class AxisScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //First change the scale of each axis object into the public scale value
 
         XBody.transform.localScale = new Vector3(1,scale.x,1);
         YBody.transform.localScale = new Vector3(1, scale.y, 1);
         ZBody.transform.localScale = new Vector3(1, scale.z, 1);
 
+        //Then we change the positions of the axis bodies, because centering in Unity sucks. WARNING: DO NOT CHANGE THE NUMBERS HERE
 
         XAxis.transform.localPosition = new Vector3(scale.x/4 - (1 / (float)5.5), ((float)0.146)+0, 0);
         YAxis.transform.localPosition = new Vector3(0, ((float)0.146)+scale.y/4-(1/ (float)5.5), 0);
@@ -66,6 +72,9 @@ public class AxisScript : MonoBehaviour
         //XBody.transform.localScale = scale;
         //YBody.transform.localScale = scale;
         //ZBody.transform.localScale = scale;
+
+        //Finally, we change the positions of the axis tips
+
         XTip.transform.localPosition = new Vector3(0, 0, scale.x / 20 -((float).05));
         YTip.transform.localPosition = new Vector3( 0, scale.y / 20 - ((float).05), 0);
         ZTip.transform.localPosition = new Vector3( scale.z / 20 - ((float).05), 0,0);
