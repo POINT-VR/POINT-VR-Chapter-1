7e1e1d87f729d9957ae40d5d4ea58bddc96d62d5
sonalij3 <sonalij3@illinois.edu>
2023-04-30 21:19:04 -0500

    Fixed broken items in feature-3.scene
    * Revamped UI elements with new design elements
    * Put in new player calling

--- POINT-VR-Chapter-1/Assets/POINT/Feature3/Constrains.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Feature3/Constrains.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class Constrains : MonoBehaviour
 {
     List<Rigidbody> spheres = new List<Rigidbody>();
-    public MeshDeformScript deformScript;
+    public GridScript deformScript;
 
     private void OnTriggerEnter(Collider other)
     {
