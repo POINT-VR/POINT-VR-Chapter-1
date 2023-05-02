69b963246b2edf8e1ca0f62a431361c33c5655c3
Chris Vistian <christophervistian@gmail.com>
2023-05-02 01:15:20 -0500

    Comments on two scripts
    see wiki

--- POINT-VR-Chapter-1/Assets/POINT/InputAssets/SceneController.cs
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/SceneController.cs
@@ -1,16 +1,49 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+/// <summary>
+/// This script should be on the player prefab. It communicates the player's setting to the GameManager before switching scenes and then retrieves this information upon being instantiated.
+/// </summary>
 public class SceneController : MonoBehaviour
 {
+    /// <summary>
+    /// The audiosource corresponding to the game's music. This should be on the player's head.
+    /// </summary>
     [SerializeField] AudioSource music;
+    /// <summary>
+    /// The menu that first appears telling the player about the game's controls.
+    /// </summary>
     [SerializeField] GameObject gripNotice;
+    /// <summary>
+    /// The UI slider for 'functional' audio
+    /// </summary>
     [SerializeField] Slider functional;
+    /// <summary>
+    /// The UI slider for 'aesthetic' audio
+    /// </summary>
     [SerializeField] Slider aesthetic;
+    /// <summary>
+    /// The class responsible for the player prefab's UI controls
+    /// </summary>
     [SerializeField] UIManager uiManager;
+    /// <summary>
+    /// The parent transform of the language select toggle gameobjects
+    /// </summary>
     [SerializeField] Transform toggleBase;
+    /// <summary>
+    /// The script that pauses the game
+    /// </summary>
     [SerializeField] PauseController pause;
-    private bool floorEnabled;
+    // The actual floor visiblilty is tracked by the UI Manager, but this variable is for inter-scene communication relating to floor visibility
+    private bool floorVisible;
+    /// <summary>
+    /// Setter called by the UI components that alters the bookkeeping of floor visibility
+    /// </summary>
+    /// <param name="enabled">Whether or not the floor is visible</param>
+    public bool FloorVisibility { set { floorVisible = value; } }
+    /// <summary>
+    /// When Instantiated: Get the player data. Communicate this data to all component arguments.
+    /// </summary>
     private void Start()
     {
         GameManager.PlayerData data = GameManager.Instance.GetData();
@@ -20,9 +53,13 @@ public class SceneController : MonoBehaviour
         aesthetic.value = data.aestheticVolume;
         uiManager.ActivateLanguageToggle(toggleBase.GetChild((int)GameManager.Instance.languageSelected).gameObject);
         music.mute = false;
-        floorEnabled = data.floorRendered;
-        uiManager.ActivateFloorToggle(floorEnabled);
+        floorVisible = data.floorRendered;
+        uiManager.ActivateFloorToggle(floorVisible);
     }
+    /// <summary>
+    /// Saves the player data to the GameManager and loads a new scene
+    /// </summary>
+    /// <param name="scene">The scene index as assigned in the build settings</param>
     public void ChangeScene(int scene)
     {
         GameManager.PlayerData data = new GameManager.PlayerData
@@ -31,14 +68,10 @@ public class SceneController : MonoBehaviour
             gripNoticeEnabled = gripNotice.activeSelf,
             functionalVolume = functional.value,
             aestheticVolume = aesthetic.value,
-            floorRendered = floorEnabled
+            floorRendered = floorVisible
         };
         GameManager.Instance.SetData(data);
         pause.Unpause();
         SceneManager.LoadScene(scene);
-    }
-    public void UpdateFloor(bool enabled)
-    {
-        floorEnabled = enabled;
     }
 }
--- POINT-VR-Chapter-1/Assets/POINT/InputAssets/XRHardwareController.cs
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/XRHardwareController.cs
@@ -25,11 +25,17 @@ public class XRHardwareController : MonoBehaviour
     /// </summary>
     [SerializeField] InputActionReference rotationReference;
     /// <summary>
-    /// The height of the player in the game world.
+    /// The height of the player in the game world. This is instantiated according to the height of the camera.
     /// </summary>
     private float playerHeight;
+    // This represents a controller that can receive haptic feedback
     private XRControllerWithRumble inputDevice;
+    // This is the distance the headset has travelled from where the player began playing the simulation.
+    // In the intended/most common use case, this should be equal to Vector3.zero
     private static Vector3 offset;
+    /// <summary>
+    /// Assigns input actions and locates the connected hardware device associated with this instance
+    /// </summary>
     private void OnEnable()
     {
         playerHeight = transform.position.y;
@@ -56,11 +62,17 @@ public class XRHardwareController : MonoBehaviour
         inputDevice.SendImpulse(0.15f, 0.05f);
 #pragma warning restore CS0162 // Unreachable code detected
     }
+    /// <summary>
+    /// Disables the actions as they are no longer used
+    /// </summary>
     private void OnDisable()
     {
         positionReference.action.Disable();
         rotationReference.action.Disable();
     }
+    /// <summary>
+    /// Manages the movement of the controllers on a frame-by-frame basis
+    /// </summary>
     void Update()
     {
         if (hardwareType == Hardware.Headset) //The headset will not update in Update()
@@ -69,6 +81,9 @@ public class XRHardwareController : MonoBehaviour
         }
         UpdateHardware();
     }
+    /// <summary>
+    /// Manages the movement of the headset on a frame-by-frame basis
+    /// </summary>
     void LateUpdate()
     {
         if (hardwareType == Hardware.Headset) //Only the headset will update in LateUpdate()
@@ -76,9 +91,11 @@ public class XRHardwareController : MonoBehaviour
             UpdateHardware();
         }
     }
+    /// <summary>
+    /// Syncs up the hardware gameobjects in the simulation with the hardware in the real world
+    /// </summary>
     private void UpdateHardware()
     {
-        //Syncs up the hardware gameobjects in the simulation with the hardware in the real world
         Vector3 pos = positionReference.action.ReadValue<Vector3>();
         if (hardwareType == Hardware.Headset && (pos-offset).sqrMagnitude > playerHeight * playerHeight) //Player has walked away: recenters the simulation
         {
