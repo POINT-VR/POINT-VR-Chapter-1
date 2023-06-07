773fe4d9cd18ea598d41b46dded483f5241979d2
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-06-07 23:10:59 +0530

    Some Setup And Skeletal Code for Instructional Text in Scene1/Scene2

---
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/PlayerUIManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerUIManager : MonoBehaviour
+{
+
+    [Header("References")]
+    [SerializeField] private TMP_Text versionText;
+    [SerializeField] private GameObject SceneUIContainer;
+    [Header("Instructions Text")]
+    [SerializeField] private string teleportationText;
+    [SerializeField] private string turnText;
+    [SerializeField] private string grabText;
+    [SerializeField] private string pushPullText;
+    [SerializeField] private string overText;
+    [SerializeField] private string menuText;
+
+    // Cache
+    private TMP_Text instructions = null;
+    private Camera currentCamera = null;
+    private GameObject player = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instructions = controlsImage.GetComponentInChildren<TMP_Text>();
+        StartCoroutine(WaitForPlayerSpawn());
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (currentCamera != null)
+        {
+            this.transform.LookAt(currentCamera.transform);
+            this.transform.Rotate(0, 180, 0);
+        }
+    }
+
+    IEnumerator WaitForPlayerSpawn()
+    {
+        yield return new WaitUntil(() => Camera.current != null);
+
+        // Start menu initialization
+        currentCamera = Camera.current;
+        this.GetComponent<Canvas>().worldCamera = currentCamera;
+        player = currentCamera.transform.parent.gameObject;
+
+
+        yield break;
+    }
+}
