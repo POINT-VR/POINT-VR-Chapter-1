01f70b37aae642988b98b2b77725a1db9c757deb
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-06-20 23:35:53 +0530

    Documented PlayerUIManager.cs

--- POINT-VR-Chapter-1/Assets/POINT/InputAssets/PlayerUIManager.cs
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/PlayerUIManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 
 public class PlayerUIManager : MonoBehaviour
 {
-
+    //Some variables which could be controlled by sliders and all in the Scene Viewer
     [Header("References")]
     [SerializeField] private GameObject SceneUIContainer;
     [SerializeField] private Image controlsImage;
@@ -30,11 +30,11 @@ public class PlayerUIManager : MonoBehaviour
     {
         if (controlsImage != null)
         {
-            instructions = controlsImage.GetComponentInChildren<TMP_Text>();
-            controlsImage.gameObject.SetActive(false);
+            instructions = controlsImage.GetComponentInChildren<TMP_Text>();//Fetches the instructions text object from the scene and stores in in the cache for reference
+            controlsImage.gameObject.SetActive(false);//Disables the controls text container
         }
-        instructions = controlsImage.GetComponentInChildren<TMP_Text>();
-        StartCoroutine(WaitForPlayerSpawn());
+        instructions = controlsImage.GetComponentInChildren<TMP_Text>();//Redundant, see the declaration above
+        StartCoroutine(WaitForPlayerSpawn());//starts the player spawn coroutine defined below
     }
 
     // Update is called once per frame
@@ -42,33 +42,33 @@ public class PlayerUIManager : MonoBehaviour
     {
         if (currentCamera != null)
         {
-            this.transform.LookAt(currentCamera.transform);
-            this.transform.Rotate(0, 180, 0);
+            this.transform.LookAt(currentCamera.transform);//syncs transform of UI container with the Active Camera (Player Camera)
+            this.transform.Rotate(0, 180, 0);//syncs rotation of UI container with the Active Camera (Player Camera)
         }
     }
 
     IEnumerator WaitForPlayerSpawn()
     {
-        yield return new WaitUntil(() => Camera.current != null);
+        yield return new WaitUntil(() => Camera.current != null);//waits until camera/player is spawned, then executes code below
 
         // Start menu initialization
-        currentCamera = Camera.current;
-        this.GetComponent<Canvas>().worldCamera = currentCamera;
-        player = currentCamera.transform.parent.gameObject;
-        this.transform.SetParent(player.transform.parent, false);
+        currentCamera = Camera.current;//fetches the Active Camera from the scene, then stores it in the cache for reference
+        this.GetComponent<Canvas>().worldCamera = currentCamera;//sets Active Camera for UI objects to the current camera
+        player = currentCamera.transform.parent.gameObject;//fetches the Player from the scene, then stores it in the cache for reference
+        this.transform.SetParent(player.transform.parent, false);//sets the Player as the parent of the object this script is attached to
 
 
 
-        StartCoroutine(WaitUntilTime());
+        StartCoroutine(WaitUntilTime());//starts the wait until time coroutine defined below
 
         yield break;
     }
     IEnumerator WaitUntilTime()
     {
-        instructions.text = instructionText;
-        controlsImage.gameObject.SetActive(true);
-        yield return new WaitForSeconds(waitPeriod);
-        controlsImage.gameObject.SetActive(false);
+        instructions.text = instructionText;//sets the text content of the text object to the instructionsText we set from the Properties tab in Scene Viewer
+        controlsImage.gameObject.SetActive(true);//enable the UI container, making it visible
+        yield return new WaitForSeconds(waitPeriod);//waits until the waitPeriod (in seconds in real time) is elapsed, then executes code below
+        controlsImage.gameObject.SetActive(false);//disable the UI container, making it invisible
         yield break;
     }
 }
