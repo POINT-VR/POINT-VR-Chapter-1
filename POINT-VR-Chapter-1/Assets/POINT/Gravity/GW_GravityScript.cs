1bf6c898eb67685f400603b56acc2d6d693b9c7b
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-07-04 00:58:16 +0530

    Documentation and Additional Functions

--- POINT-VR-Chapter-1/Assets/POINT/Gravity/GW_GravityScript.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Gravity/GW_GravityScript.cs
@@ -22,7 +22,14 @@ public class GW_GravityScript : MonoBehaviour
 
     public Vector3 MovePlusMode(Vector3 pos, Vector3 center, float angle)
     {
+        /**
+         * Motion of a particle based on Plus Mode polarization of a gravitational wave
+         * Works on the principle of x = x_0 + (direction of normal to the mesh) * cos(omega * t)
+         * And y = y_0 - (direction of normal to the mesh) * cos(omega * t)
+         * (Gives out both x and y oscillations, given propagation on z axis)
+         * **/
 
+        //Create normal vector of mesh; mesh is translated along this direction
         Vector3 unitVector = (pos - center).normalized;
 
 
@@ -39,7 +46,14 @@ public class GW_GravityScript : MonoBehaviour
 
     public Vector3 MoveCrossMode(Vector3 pos, Vector3 center, float angle)
     {
+        /**
+         * Motion of a particle based on Cross Mode polarization of a gravitational wave
+         * Works on the principle of x = x_0 - (direction of tangent to the mesh) * sin(omega * t)
+         * And y = y_0 + (direction of tangent to the mesh) * sin(omega * t)
+         * (Gives out both x and y oscillations, given propagation on z axis)
+         * **/
 
+        //Create tangent vector of mesh; mesh is translated along this direction
         Vector3 unitVectory = (pos-center).normalized;
         Vector3 unitVector = new Vector3(-unitVectory.y, unitVectory.x, 0);
 
@@ -49,6 +63,56 @@ public class GW_GravityScript : MonoBehaviour
         - unitVector.x * amplitude * Mathf.Sin(Time.time * speed * Mathf.Deg2Rad / Mathf.PI);
         output.y = pos.y
             + unitVector.y * amplitude * Mathf.Sin(Time.time * speed * Mathf.Deg2Rad / Mathf.PI);
+        output.z = pos.z;
+
+        return output;
+    }
+
+    public Vector3 MovePlusMode(Vector3 pos, Vector3 center, float angle, float phase)
+    {
+        /**
+         * -----------Additional Method of the Function with Phase of the Oscillations-----------
+         * Motion of a particle based on Plus Mode polarization of a gravitational wave
+         * Works on the principle of x = x_0 + (direction of normal to the mesh) * cos(omega * t + phi)
+         * And y = y_0 - (direction of normal to the mesh) * cos(omega * t + phi)
+         * (Gives out both x and y oscillations, given propagation on z axis)
+         * **/
+
+        //Create normal vector of mesh; mesh is translated along this direction
+        Vector3 unitVector = (pos - center).normalized;
+
+
+
+        Vector3 output;
+        output.x = pos.x
+            + unitVector.x * amplitude * Mathf.Cos(Time.time * speed * Mathf.Deg2Rad / Mathf.PI + phase);
+        output.y = pos.y
+            - unitVector.y * amplitude * Mathf.Cos(Time.time * speed * Mathf.Deg2Rad / Mathf.PI + phase);
+        output.z = pos.z;
+
+        return output;
+    }
+
+    public Vector3 MoveCrossMode(Vector3 pos, Vector3 center, float angle, float phase)
+    {
+        /**
+         *  -----------Additional Method of the Function with Phase of the Oscillations-----------
+         * Motion of a particle based on Cross Mode polarization of a gravitational wave
+         * Works on the principle of x = x_0 - (direction of tangent to the mesh) * sin(omega * t + phi)
+         * And y = y_0 + (direction of tangent to the mesh) * sin(omega * t + phi)
+         * (Gives out both x and y oscillations, given propagation on z axis)
+         * **/
+
+        //Create tangent vector of mesh; mesh is translated along this direction
+        Vector3 unitVectory = (pos - center).normalized;
+        Vector3 unitVector = new Vector3(-unitVectory.y, unitVectory.x, 0);
+
+
+        Vector3 output;
+        output.x = pos.x
+        - unitVector.x * amplitude * Mathf.Sin(Time.time * speed * Mathf.Deg2Rad / Mathf.PI + phase);
+        output.y = pos.y
+            + unitVector.y * amplitude * Mathf.Sin(Time.time * speed * Mathf.Deg2Rad / Mathf.PI + phase);
         output.z = pos.z;
 
         return output;
--- POINT-VR-Chapter-1/Assets/POINT/Gravity/GW_Ring.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Gravity/GW_Ring.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GW_Ring : MonoBehaviour
 {
-
+    
     [SerializeField] private GameObject SphereMesh;
     [SerializeField] public int numberOfMeshes = 12;
     [SerializeField] public float radius = 5.0f;
@@ -21,27 +21,37 @@ public class GW_Ring : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        
         gw_gravity = GetComponent<GW_GravityScript>();
         
+        //Initialize mesh and angles array, to create our coordinate system which we use to translate particles
         sphere_array = new List<GameObject>(numberOfMeshes);
         angles_array = new List<float>(numberOfMeshes);
         sphere_pos_array = new List<Vector3>(numberOfMeshes);
 
+        //Center position of the GameObject this script is attached to, used to spawn particles around it
         Vector3 center = transform.position;
 
+        //Setting up angular step for the ring of particles, based on the number of meshes
         float global_ang = 360.0f / numberOfMeshes;
 
         for(int i = 0; i < numberOfMeshes; i++)
         {
+            //Set up angular distance of each particle in the ring
             float a = i * global_ang;
+
+            //Arranges the system of particles and gives a coordinate of the particle based on a circle with a center and radius given, definition below
             Vector3 pos = SpawnCircle(center, radius, a);
             
-
+            //Instantiate particle mesh prefab and store particle GameObject, particle position, and angular distance of the particle in our coordinate arrays
             GameObject instance = Instantiate(SphereMesh, pos, Quaternion.identity) as GameObject;
             sphere_array.Add(instance);
             sphere_pos_array.Add(pos);
-            instance.transform.parent = ring.transform;
             angles_array.Add(a);
+
+            //Parents the particle to the ring GameObject
+            instance.transform.parent = ring.transform;
+            
         }
     }
 
@@ -58,16 +68,21 @@ public class GW_Ring : MonoBehaviour
 
             //Vector3 pos = gw_gravity.MoveCrossMode(sphere_pos_array[i], ring.transform.position, angles_array[i]);
 
+            //Gives coordinates of oscillations of particles based on plus mode and cross mode polarizations of gravitational waves, can be controlled
+            //Phasor addition of Plus and Cross Mode polarization - based oscillations used
             Vector3 pos = PercentOfPlusMode/100.0f* gw_gravity.MovePlusMode(sphere_pos_array[i], ring.transform.position, angles_array[i]) 
                 + PercentOfCrossMode / 100.0f* gw_gravity.MoveCrossMode(sphere_pos_array[i], ring.transform.position, angles_array[i]);
             pos.z = sphere_array[i].transform.position.z;
 
+            //Translates particle to the calculated coordinate
             sphere_array[i].transform.position = pos;
         }
     }
 
     Vector3 SpawnCircle(Vector3 center, float radius, float ang)
     {
+
+        //Generates a coordinate based on a parametric equation of a circle based on the center, radius, and angular distance given
         Vector3 pos;
 
         pos.x = center.x + radius * Mathf.Sin(ang * Mathf.Deg2Rad);
