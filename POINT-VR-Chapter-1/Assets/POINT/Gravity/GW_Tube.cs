dca5d4c44aeeb079106ba3564488d1ea3ee49d59
EnderNinja7 <dhruvsrivastava.2@gmail.com>
2023-07-17 02:43:31 +0530

    Set up skeletal code for Tube of Rings of Particles

---
+++ POINT-VR-Chapter-1/Assets/POINT/Gravity/GW_Tube.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GW_Tube : MonoBehaviour
+{
+
+    [SerializeField] private GameObject RingMesh;
+    [SerializeField] public int numberOfMeshes = 12;
+    [SerializeField] public float radius = 5.0f;
+    [SerializeField] private GameObject tube;
+    [SerializeField] private float PercentOfPlusMode;
+    [SerializeField] private float PercentOfCrossMode;
+    [SerializeField] private float PercentOfBreathingMode;
+    [SerializeField] private float phaseDifference;
+    [SerializeField] private float ampStep;
+
+    private List<GameObject> ring_array;
+    private List<float> phase_array;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
