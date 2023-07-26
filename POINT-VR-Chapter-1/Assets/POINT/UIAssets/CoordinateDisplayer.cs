beacea8bb05c6ebdbb75db1a40d4fa3a339c412f
George Bayliss <calculanator5@gmail.com>
2023-07-26 17:18:06 -0500

    Created adjustable 2d Coordinate plane
    Also minor improvements to coordinate displayer

--- POINT-VR-Chapter-1/Assets/POINT/UIAssets/CoordinateDisplayer.cs
+++ POINT-VR-Chapter-1/Assets/POINT/UIAssets/CoordinateDisplayer.cs
@@ -6,18 +6,39 @@ using System;
 
 public class CoordinateDisplayer : MonoBehaviour
 {
-    private bool showTime = false;
+    /// <summary>
+    /// Decides whether or not to include the 4th time coordinate. 
+    /// </summary>
+    private bool showTime;
 
+    /// <summary>
+    /// Stores the origin for time, t=0.
+    /// </summary>
     private float startTime;
 
+    /// <summary>
+    /// Stores refrence to TMP object which will display the coordinates.
+    /// </summary>
     [SerializeField]
     TMP_Text coordinateText;
 
+    /// <summary>
+    /// The origin spacial coordinates.
+    /// </summary>
+    [SerializeField]
     Vector3 origin;
+
+    /// <summary>
+    /// Decides whether or not time is enabled upon start
+    /// </summary>
+    [SerializeField]
+    bool showTimeOnStart;
+
     // Start is called before the first frame update
     void Start()
     {
         startTime = Time.time;
+        showTime = showTimeOnStart;
     }
 
     // Update is called once per frame
@@ -26,13 +47,14 @@ public class CoordinateDisplayer : MonoBehaviour
         Vector3 outputCoordinates = transform.position - origin;
         if (showTime)
         {
-            coordinateText.text = $"({Math.Floor(Time.time - startTime)}, {outputCoordinates.x}, {outputCoordinates.y}, {outputCoordinates.z})";
+            coordinateText.text = $"({outputCoordinates.x}, {outputCoordinates.y}, {outputCoordinates.z}, {Math.Floor(Time.time - startTime)})";
         }
         else
         {
             coordinateText.text = $"({outputCoordinates.x}, {outputCoordinates.y}, {outputCoordinates.z})";
         }
     }
+    //Public member functions to be accessed by other scripts
     public void ShowTime()
     {
         showTime = true;
@@ -41,8 +63,7 @@ public class CoordinateDisplayer : MonoBehaviour
     {
         showTime = false;
     }
-
-    public void SetTime(float offset) 
+    public void SetTime(float offset) //sets the time coordinate to any value
     {
         startTime = Time.time - offset; //if we want t=x in the present then we make the startTime x seconds before the present
     }
