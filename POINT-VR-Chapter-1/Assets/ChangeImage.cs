2bd97eee61c78b4dca2f2083cda4f4806f4d8a60
jrang3 <jrang3@illinois.edu>
2023-07-31 17:56:47 -0500

    Started to work on feature to allow users to change mode
    So far just created images of button and placed corresponding text. Need to work on changing spirte when pressed as well as change percent modes

---
+++ POINT-VR-Chapter-1/Assets/ChangeImage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Sprite newButtonImage;
+    public Button button;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        button.image.sprite = newButtonImage;
+        
+    }
+}
