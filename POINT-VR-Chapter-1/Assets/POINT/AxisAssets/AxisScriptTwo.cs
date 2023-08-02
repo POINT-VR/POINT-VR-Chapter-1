22fe65a931d9f8dc443ce6752366ccdca7a4aa93
George Bayliss <calculanator5@gmail.com>
2023-08-01 19:10:34 -0500

    Floating menu + some work on axes

---
+++ POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisScriptTwo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AxisScriptTwo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.localScale = this.transform.localScale + new Vector3(0, (float)0.05, 0);
+        this.transform.localPosition = this.transform.localPosition + (float)0.05 * transform.up;
+
+
+    }
+}
---
+++ POINT-VR-Chapter-1/Assets/POINT/UIAssets/FloatingObjectives.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloatingObjectives : MonoBehaviour
+{
+    private Camera cameraObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraObject = Camera.allCameras[0];
+        this.transform.SetParent(cameraObject.transform);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
