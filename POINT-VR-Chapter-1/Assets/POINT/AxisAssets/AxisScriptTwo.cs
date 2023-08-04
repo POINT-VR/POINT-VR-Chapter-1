3feeadeb137dc76dae32399733dbed2e3fd4621f
George Bayliss <calculanator5@gmail.com>
2023-08-03 22:04:38 -0500

    Improvements to axis script

--- POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisScriptTwo.cs
+++ POINT-VR-Chapter-1/Assets/POINT/AxisAssets/AxisScriptTwo.cs
@@ -1,21 +1,54 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class AxisScriptTwo : MonoBehaviour
 {
+    public MeshRenderer xAxis;
+    public MeshRenderer yAxis;
+    public MeshRenderer zAxis;
+    public float startLength;
+    public float endLength;
+    public float speed;
+
+    float timeElapsed = 0;
+    float duration;
     // Start is called before the first frame update
     void Start()
     {
-        
+        duration = (endLength - startLength) / speed;
+        SetAxisLength(startLength);
+        xAxis.material.color = Color.red;
+        yAxis.material.color = Color.green;
+        zAxis.material.color = Color.blue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.transform.localScale = this.transform.localScale + new Vector3(0, (float)0.05, 0);
-        this.transform.localPosition = this.transform.localPosition + (float)0.05 * transform.up;
+        if (timeElapsed < duration)
+        {
+            float t = timeElapsed / duration;
+            float lerpPoint = Mathf.Lerp(startLength, endLength, t);
+            timeElapsed += Time.deltaTime;
+            SetAxisLength(lerpPoint);
+        }
+        else
+        {
+            SetAxisLength(endLength);
+        }
+    }
+    void SetAxisLength(float length)
+    {
+        xAxis.transform.localScale = new Vector3(xAxis.transform.localScale.x, length, xAxis.transform.localScale.z);
+        xAxis.transform.localPosition = length * xAxis.transform.up;
 
+        yAxis.transform.localScale = new Vector3(yAxis.transform.localScale.x, length, yAxis.transform.localScale.z);
+        yAxis.transform.localPosition = length * yAxis.transform.up;
 
+        zAxis.transform.localScale = new Vector3(zAxis.transform.localScale.x, length, zAxis.transform.localScale.z);
+        zAxis.transform.localPosition = length * zAxis.transform.up;
     }
 }
+
