6eb6536d29f14b1928b3068bf179579eebed09e8
sonalij3 <sonalij3@illinois.edu>
2024-10-03 13:10:40 -0500

    ArrowTask now waits to be completed before scene continues
    Had to put the wait in the main coroutine and have the arrow task check in the Update function. state is a variable that checks each frame if the arrows are pointing the right direction.

--- POINT-VR-Chapter-1/Assets/POINT/Scenes/Conference Demos/ConfDemo_part1_one_mass_y_manager.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Scenes/Conference Demos/ConfDemo_part1_one_mass_y_manager.cs
@@ -27,16 +27,57 @@ public class ConfDemo_part1_one_mass_y_manager : MonoBehaviour
     private GameObject buttons = null;
     private UIManager UIManagerScript = null;
 
+    private GameObject arrow0 = null;
+    private GameObject arrow1 = null;
+    private GameObject arrow2 = null;
+    private GameObject arrow3 = null;
+    private GameObject arrow4 = null;
+    private GameObject arrow5 = null;
+    private bool arrow0_state = false;
+    private bool arrow1_state = false;
+    private bool arrow2_state = false;
+    private bool arrow3_state = false;
+    private bool arrow4_state = false;
+    private bool arrow5_state = false;
+    private bool state = false;
+    private bool arrowTask = false;
+
     private void Start()
     {
         SceneUIContainer.SetActive(false);
         snapRing.SetActive(false);
         StartCoroutine(WaitForPlayerSpawn());
         setOfDirectionalArrows.SetActive(false);
+
+        arrow0 = setOfDirectionalArrows.transform.Find("Directional Arrow").gameObject;
+        arrow1 = setOfDirectionalArrows.transform.Find("Directional Arrow (1)").gameObject;
+        arrow2 = setOfDirectionalArrows.transform.Find("Directional Arrow (2)").gameObject;
+        arrow3 = setOfDirectionalArrows.transform.Find("Directional Arrow (3)").gameObject;
+        arrow4 = setOfDirectionalArrows.transform.Find("Directional Arrow (4)").gameObject;
+        arrow5 = setOfDirectionalArrows.transform.Find("Directional Arrow (5)").gameObject;
     }
 
     private void Update()
-    {
+    {   
+        /*
+        // TODO: Maybe move all these check to a Coroutine so it isn't always checking the state of these arrows
+        // Tried a version of a Coroutine, but could not get 
+        // yield return new WaitUntil(() => state != false) to work properly
+        */
+        arrow0_state = arrow0.GetComponent<Directional_Arrow_Script>().Correct;
+        arrow1_state = arrow1.GetComponent<Directional_Arrow_Script>().Correct;
+        arrow2_state = arrow2.GetComponent<Directional_Arrow_Script>().Correct;
+        arrow3_state = arrow3.GetComponent<Directional_Arrow_Script>().Correct;
+        arrow4_state = arrow4.GetComponent<Directional_Arrow_Script>().Correct;
+        arrow5_state = arrow5.GetComponent<Directional_Arrow_Script>().Correct;
+        state = arrow0_state & arrow1_state & arrow2_state & arrow3_state & arrow4_state & arrow5_state;
+
+        // TODO: Also move this to the ArrowDirectionTask Coroutine
+        if (arrowTask == true)
+        {
+            massSphere.GetComponent<Rigidbody>().velocity = Vector3.zero; // move massSphere to center of grid
+            massSphere.transform.position = new Vector3(4.5f, 2f, 4f); // move massSphere to center of grid
+        }
     }
 
     IEnumerator WaitForPlayerSpawn()
@@ -70,53 +111,43 @@ public class ConfDemo_part1_one_mass_y_manager : MonoBehaviour
         // Get UI Manager UIManagerScript
         UIManagerScript = Menu.GetComponent<UIManager>();
 
-        yield return new WaitForSecondsRealtime(1);
-        /*
         UIManagerScript.updateCurrentObjective(objective1); // Grid/Space Deformation
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\1_intro_to_grids_and_clocks_1");
-        yield return new WaitForSecondsRealtime(16);
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\1_intro_to_grids_and_clocks_2");
-        yield return new WaitForSecondsRealtime(7);
 
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\2_observe_grid_curve_1");
-        yield return new WaitForSecondsRealtime(60); // Pause to let player explore
 
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_1");
-        yield return new WaitForSecondsRealtime(10);
-        */
         // Question Time for Player: What does that say about Gravity
-        // TODO: Would be cool if after this question the sun sphere animated to the center of the grid. 
+
         UIManagerScript.updateCurrentObjective(objective2); 
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_2");
-        yield return new WaitForSecondsRealtime(7);
-        // move massSphere to center of grid
-        massSphere.transform.position = new Vector3(4.5f, 2f, 4f);
-        setOfDirectionalArrows.SetActive(true); 
-        // set massSphere to uninteractable
-        massSphere.layer = LayerIgnoreRayCast;
-        // spawn six directional arrows
+        
+        // TODO: Would be cool if after this question the sun sphere animated to the center of the grid. 
+        // Instead of just spawing at the center of the grid
+        massSphere.transform.parent = null; // Make player drop the sphere
+        arrowTask = true; // See Update function, makes sphere stop moving and locates it to the center of the grid
+        setOfDirectionalArrows.SetActive(true);  // spawn six directional arrows
+        massSphere.layer = LayerIgnoreRayCast; // set massSphere to uninteractable
+                
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_3");
-        yield return new WaitForSecondsRealtime(2.5f);
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_4");
-        yield return new WaitForSecondsRealtime(2);
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_5");
-        //yield return new WaitForSecondsRealtime(6.1);
-        // Complete objective
-        StartCoroutine(CorrectArrowDirectionTask());
+        yield return new WaitForSeconds(6.1f);
+        
+        yield return new WaitUntil(() => state != false); // Complete objective
+        
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_6");
-        yield return new WaitForSecondsRealtime(1); // Nice Job
 
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_7");
-        yield return new WaitForSecondsRealtime(22);
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\3_direction_of_curvature_arrow_8");
-        yield return new WaitForSecondsRealtime(5.5f);
         setOfDirectionalArrows.SetActive(false);
         massSphere.layer = LayerGrip; // End of Task, set sphere back to interactable
+        arrowTask = false;
 
         UIManagerScript.updateCurrentObjective(objective3); // Time Deformation
         snapRing.SetActive(true);
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\4_observe_time_dialation_1");
-        yield return new WaitForSecondsRealtime(8);
         player.GetComponent<NarrationManager>().PlayClipWithSubtitles("Chapter1Scene2\\4_observe_time_dialation_2");
 
         yield return new WaitUntil(() => snapRing.GetComponentInChildren<Rigidbody>() != null);
@@ -126,44 +157,5 @@ public class ConfDemo_part1_one_mass_y_manager : MonoBehaviour
         SceneUIContainer.SetActive(true); // Continue to next scene 
 
         yield break;
-    }
-
-    public IEnumerator CorrectArrowDirectionTask()
-    {
-        GameObject arrow0 = setOfDirectionalArrows.transform.Find("Directional Arrow").gameObject;
-        bool arrow0_state = arrow0.GetComponent<Directional_Arrow_Script>().Correct;
-
-        GameObject arrow1 = setOfDirectionalArrows.transform.Find("Directional Arrow (1)").gameObject;
-        arrow1.GetComponent<Directional_Arrow_Script>().test();
-        arrow1.GetComponent<Directional_Arrow_Script>().test();
-        bool arrow1_state = arrow1.GetComponent<Directional_Arrow_Script>().Correct;
-
-        GameObject arrow2 = setOfDirectionalArrows.transform.Find("Directional Arrow (2)").gameObject;
-        arrow2.GetComponent<Directional_Arrow_Script>().test();
-        arrow2.GetComponent<Directional_Arrow_Script>().test();
-        arrow2.GetComponent<Directional_Arrow_Script>().test();
-        bool arrow2_state = arrow2.GetComponent<Directional_Arrow_Script>().Correct;
-
-        GameObject arrow3 = setOfDirectionalArrows.transform.Find("Directional Arrow (3)").gameObject;
-        arrow3.GetComponent<Directional_Arrow_Script>().test();
-        Debug.Log(arrow3.GetComponent<Directional_Arrow_Script>().Correct);
-        bool arrow3_state = arrow3.GetComponent<Directional_Arrow_Script>().Correct;
-
-        GameObject arrow4 = setOfDirectionalArrows.transform.Find("Directional Arrow (4)").gameObject;
-        arrow4.GetComponent<Directional_Arrow_Script>().test();
-        arrow4.GetComponent<Directional_Arrow_Script>().test();
-        arrow4.GetComponent<Directional_Arrow_Script>().test();
-        bool arrow4_state = arrow4.GetComponent<Directional_Arrow_Script>().Correct;
-
-        GameObject arrow5 = setOfDirectionalArrows.transform.Find("Directional Arrow (5)").gameObject;
-        arrow5.GetComponent<Directional_Arrow_Script>().test();
-        Debug.Log(arrow5.GetComponent<Directional_Arrow_Script>().Correct);
-        bool arrow5_state = arrow5.GetComponent<Directional_Arrow_Script>().Correct;
-
-        bool state = arrow0_state & arrow1_state & arrow2_state & arrow3_state & arrow4_state & arrow5_state;
-        Debug.Log(state);
-
-        if ( state == true)
-            yield break;
     }
 }
