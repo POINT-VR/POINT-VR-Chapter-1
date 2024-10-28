3cee6cbb7fefe4fa3eb2d490b7f4e35042cee562
Dante Ratchev <104606374+RatchevD@users.noreply.github.com>
2024-10-27 22:59:27 -0500

    Initial Testing Scene Setup
    Set up the Density Exploration Scene where I will be making all of the changes, and added in the beginning critical components such that it matches the other scenes.

---
+++ POINT-VR-Chapter-1/Assets/POINT/Scenes/Chapter1/DensityExploration.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class DensityExploration : MonoBehaviour
+{
+    // Serialized fields
+    [Header("References")]
+    [SerializeField] private GameObject floor;
+    [SerializeField] private InputActionReference openMenuReference;
+    // Cache
+    private Camera currentCamera = null;
+    private GameObject player = null;
+    private GameObject menus = null;
+    private GameObject buttons = null;
+    private UIManager UIManagerScript = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(WaitForPlayerSpawn());
+    }
+    private IEnumerator WaitForPlayerSpawn()
+    {
+        yield return new WaitUntil(() => Camera.current != null);
+
+        // Start menu initialization
+        currentCamera = Camera.current;
+        player = currentCamera.transform.parent.gameObject;
+
+        StartCoroutine(StartScene());
+    }
+    void Update()
+    {
+        
+    }
+    IEnumerator StartScene()
+    {
+        //Instantiate menus from player prefab and buttons from player prefab as well
+        GameObject mainCamera = player.transform.Find("Main Camera").gameObject;
+        GameObject UIContainer = mainCamera.transform.Find("UI Container").gameObject;
+        GameObject Menu = UIContainer.transform.Find("Menu").gameObject;
+        GameObject HeaderButtons = Menu.transform.Find("HeaderButtons").gameObject;
+        // GameObject HeaderButtons = Menu.transform.Find("Buttons").gameObject; //if testing with emulator use this
+        GameObject menuScreens = Menu.transform.Find("MenuScreens").gameObject;
+
+        menus = menuScreens;
+        buttons = HeaderButtons;
+
+        // Get UI Manager UIManagerScript
+        UIManagerScript = Menu.GetComponent<UIManager>();
+        
+        yield return new WaitForSecondsRealtime(100);
+        
+        yield break;
+    }
+
+}
