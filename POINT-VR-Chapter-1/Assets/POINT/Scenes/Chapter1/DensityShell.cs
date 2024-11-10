48dfc1c9c4048527cd7b70464e479c6809e2d34c
Dante Ratchev <104606374+RatchevD@users.noreply.github.com>
2024-11-10 00:31:49 -0600

    Created DensityShell script
    Created Script For the Density Shell That Takes Care of the Updating Material Process based on the value of the checkbox (which is updated by HandController). The boolean value can also easily be accessed through the SelectObject object.

---
+++ POINT-VR-Chapter-1/Assets/POINT/Scenes/Chapter1/DensityShell.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DensityShell : MonoBehaviour
+{
+    // The Selected checkbox
+    SelectObject selected;
+    // Reference to the Renderer component of the GameObject
+    private Renderer objectRenderer;
+    // The materials to switch between
+    public Material material_unselected;  // The unselected material
+    public Material material_selected; // The selected material
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        selected = GetComponent<SelectObject>();
+        objectRenderer = GetComponent<Renderer>();
+        UpdateMaterial();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMaterial();
+    }
+    private void UpdateMaterial()
+    {
+        bool selectval = selected.GetSelected();
+        if (objectRenderer != null)
+        {
+            // Change the material based on the Selected state
+            objectRenderer.material = selectval ? material_selected : material_unselected;
+        }
+    }
+}
