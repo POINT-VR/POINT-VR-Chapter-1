3ce497db00ade423037d02c0e8ebe12ee7870e2a
George Bayliss <calculanator5@gmail.com>
2024-11-26 23:18:08 -0600

    Placeholder compass UI implemented

--- POINT-VR-Chapter-1/Assets/POINT/Compass/CompassScript.cs
+++ POINT-VR-Chapter-1/Assets/POINT/Compass/CompassScript.cs
@@ -6,18 +6,41 @@ using UnityEngine.UIElements;
 
 public class CompassScript : MonoBehaviour
 {
-    [SerializeField]
-    private List<GameObject> gameObjects;
-
     /// <summary>
     /// Stores the camera which the floating objectives attaches itself too
     /// </summary>
     private Camera cameraObject;
 
+    /// <summary>
+    /// Stores tags, gameobjects, and angles
+    /// </summary>
     private List<double> angles = new List<double>();
+
+    /// <summary>
+    /// Stores all ticks
+    /// </summary>
+    private List<RectTransform> ticks = new List<RectTransform>();
 
     [SerializeField]
     private Vector3 position;
+
+    /// <summary>
+    /// Initial list which takes gameobjects to be attached
+    /// </summary>
+    [SerializeField]
+    private List<GameObject> gameObjects;
+
+    /// <summary>
+    /// Initial list which takes tages to be attached
+    /// </summary>
+    [SerializeField]
+    private List<string> gameLabels;
+
+    /// <summary>
+    /// Tick Prefab
+    /// </summary>
+    [SerializeField]
+    private GameObject tick;
 
     // Start is called before the first frame update
     void Start()
@@ -25,23 +48,56 @@ public class CompassScript : MonoBehaviour
         cameraObject = Camera.allCameras[0];
         this.transform.SetParent(cameraObject.transform);
         this.transform.localPosition = position;
-        foreach (GameObject gameObject in gameObjects)
-            angles.Add(0);
+        foreach (var obj in gameObjects)
+        {
+            angles.Add(0.0);
+            CreateTick();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        UpdatePosition();
+        UpdateTicks();
+    }
+
+    void UpdatePosition()
+    {
         int i = 0;
-        foreach (GameObject gameObject in gameObjects) 
+        foreach (var obj in gameObjects)
         {
-            Vector3 direction = gameObject.transform.position - cameraObject.transform.position;
+            Vector3 direction = obj.transform.position - cameraObject.transform.position;
             Vector3 orientation = cameraObject.transform.forward;
-            double theta1 = Math.Atan2(direction.x, direction.z)*180/Math.PI;
-            double theta2 = Math.Atan2(orientation.x, orientation.z)*180/Math.PI;
-            angles[i] = theta1-theta2;
+            double theta1 = Math.Atan2(direction.x, direction.z) * 180 / Math.PI;
+            double theta2 = Math.Atan2(orientation.x, orientation.z) * 180 / Math.PI;
+            double difference = theta1 - theta2;
+            if (difference > 180)
+            {
+                difference -= 360;
+            } else if (difference < -180)
+            {
+                difference += 360;
+            }
+            angles[i] = (difference);
             i++;
         }
-        Debug.Log(angles[0]);
+    }
+
+    void UpdateTicks()
+    {
+        int i = 0;
+        foreach (var Tick in ticks)
+        {
+            Tick.anchoredPosition3D = new Vector3((float)angles[i]*10/9, 500, 0);
+            i++;
+        }
+    }
+
+    void CreateTick()
+    {
+        var Tick = Instantiate(tick,this.transform);
+        Tick.GetComponent<RectTransform>().anchoredPosition3D = new Vector3(0, 500, 0);
+        ticks.Add(Tick.GetComponent<RectTransform>());
     }
 }
