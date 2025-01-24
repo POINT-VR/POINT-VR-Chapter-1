99a949c0c5b5b3139511b1868e512f0ba274719a
Dante Ratchev <104606374+RatchevD@users.noreply.github.com>
2025-01-23 18:17:39 -0600

    Snapping for all 8 masses
    Added the snapping and unsnapping mechanic for all 8 masses

--- POINT-VR-Chapter-1/Assets/POINT/DensityShellAssets/DensityExploration.cs
+++ POINT-VR-Chapter-1/Assets/POINT/DensityShellAssets/DensityExploration.cs
@@ -20,13 +20,13 @@ public class DensityExploration : MonoBehaviour
     private GameObject mass2 = null;
     private GameObject mass3 = null;
     // Array of Snap Locations
-    Vector3[,] positionArray = new Vector3[3, 3];
+    Vector3[,] positionArray = new Vector3[8, 8];
 
     // Number of snapped masses
     private int numSnapped = 0;
     // List of what masses are snapped
-    bool[] snapArray = new bool[3];
-    GameObject[] massList = new GameObject[3];
+    bool[] snapArray = new bool[8];
+    GameObject[] massList = new GameObject[8];
     private
     // Start is called before the first frame update
     void Start()
@@ -42,12 +42,14 @@ public class DensityExploration : MonoBehaviour
         // Start menu initialization
         currentCamera = Camera.current;
         player = currentCamera.transform.parent.gameObject;
-        mass1 = GameObject.Find("UnitMassOne");
-        mass2 = GameObject.Find("UnitMassTwo");
-        mass3 = GameObject.Find("UnitMassThree");
         massList[0] = GameObject.Find("UnitMassOne");
         massList[1] = GameObject.Find("UnitMassTwo");
         massList[2] = GameObject.Find("UnitMassThree");
+        massList[3] = GameObject.Find("UnitMassFour");
+        massList[4] = GameObject.Find("UnitMassFive");
+        massList[5] = GameObject.Find("UnitMassSix");
+        massList[6] = GameObject.Find("UnitMassSeven");
+        massList[7] = GameObject.Find("UnitMassEight");
         shell = GameObject.Find("Sphere");
         // Fill Snap List
         positionArray[0, 0] = new Vector3(0f, 0f, 0f);
@@ -56,10 +58,45 @@ public class DensityExploration : MonoBehaviour
         positionArray[2, 0] = new Vector3(-0.75f, -0.75f, 0f);
         positionArray[2, 1] = new Vector3(0.75f, -0.75f, 0f);
         positionArray[2, 2] = new Vector3(0f, 0.75f, 0f);
+        positionArray[3, 0] = new Vector3(-0.7f, -0.7f, 0f);
+        positionArray[3, 1] = new Vector3(0.7f, -0.7f, 0f);
+        positionArray[3, 2] = new Vector3(0.7f, 0.7f, 0f);
+        positionArray[3, 3] = new Vector3(-0.7f, 0.7f, 0f);
+        positionArray[4, 0] = new Vector3(-0.7f, -0.7f, 0f);
+        positionArray[4, 1] = new Vector3(-0.7f, 0.7f, 0f);
+        positionArray[4, 2] = new Vector3(0.7f, -0.7f, 0f);
+        positionArray[4, 3] = new Vector3(0.7f, 0.7f, 0f);
+        positionArray[4, 4] = new Vector3(0f, 0f, 0f);
+        positionArray[5, 0] = new Vector3(-0.7f, -0.7f, 0f);
+        positionArray[5, 1] = new Vector3(-0.7f, 0.7f, 0f);
+        positionArray[5, 2] = new Vector3(0.7f, -0.7f, 0f);
+        positionArray[5, 3] = new Vector3(0.7f, 0.7f, 0f);
+        positionArray[5, 4] = new Vector3(0f, 0f, -0.75f);
+        positionArray[5, 5] = new Vector3(0f, 0f, 0.75f);
+        positionArray[6, 0] = new Vector3(-0.7f, -0.7f, 0f);
+        positionArray[6, 1] = new Vector3(-0.7f, 0.7f, 0f);
+        positionArray[6, 2] = new Vector3(0.7f, -0.7f, 0f);
+        positionArray[6, 3] = new Vector3(0.7f, 0.7f, 0f);
+        positionArray[6, 4] = new Vector3(0f, 0f, -0.75f);
+        positionArray[6, 5] = new Vector3(0f, 0f, 0.75f);
+        positionArray[6, 6] = new Vector3(0f, 0f, 0f);
+        positionArray[7, 0] = new Vector3(-0.7f, -0.7f, -0.75f);
+        positionArray[7, 1] = new Vector3(-0.7f, 0.7f, -0.75f);
+        positionArray[7, 2] = new Vector3(0.7f, -0.7f, -0.75f);
+        positionArray[7, 3] = new Vector3(0.7f, 0.7f, -0.75f);
+        positionArray[7, 4] = new Vector3(-0.7f, -0.7f, 0.75f);
+        positionArray[7, 5] = new Vector3(-0.7f, 0.7f, 0.75f);
+        positionArray[7, 6] = new Vector3(0.7f, -0.7f, 0.75f);
+        positionArray[7, 7] = new Vector3(0.7f, 0.7f, 0.75f);
         //
         snapArray[0] = false;
         snapArray[1] = false;
         snapArray[2] = false;
+        snapArray[3] = false;
+        snapArray[4] = false;
+        snapArray[5] = false;
+        snapArray[6] = false;
+        snapArray[7] = false;
         //
         numSnapped = 0;
         StartCoroutine(StartScene());
@@ -93,36 +130,43 @@ public class DensityExploration : MonoBehaviour
     private void snapChecker() {
         // check the position of each mass
         int count = 0;
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 8; i++) {
             if (massList[i] != null) {
                 // if the mass is marked as snapped, check if it has moved from its expected position
                 if (snapArray[i] == true) {
                     if ((massList[i].transform.position != positionArray[numSnapped - 1, count]) && (massList[i].transform.parent == null || massList[i].transform.parent.gameObject.name != "Hand")) {
-                        // if mass is not in expected position
-                        numSnapped = numSnapped - 1;
-                        snapArray[i] = false;
-                        massList[i].transform.SetParent(null);
-                        int count2 = 0;
-                        for (int j = 0; j < 3; j++) {
-                            if (snapArray[j] == true) {
-                                massList[j].transform.position = positionArray[numSnapped -1, count2];
-                                massList[j].GetComponent<Rigidbody>().isKinematic = false;
-                                count2 = count2 + 1;
+                        if ((massList[i].transform.position.magnitude < 2)  && (massList[i].transform.parent == null || massList[i].transform.parent.gameObject.name != "Hand")) {
+                           int count2 = 0;
+                           for (int j = 0; j < 8; j++) {
+                                if (snapArray[j] == true) {
+                                    massList[j].transform.position = positionArray[numSnapped - 1, count2];
+                                    massList[j].GetComponent<Rigidbody>().isKinematic = true;
+                                    count2 = count2 + 1;
+                                }
+                            } 
+                        } else {
+                            numSnapped = numSnapped - 1;
+                            snapArray[i] = false;
+                            massList[i].transform.SetParent(null);
+                            int count2 = 0;
+                            for (int j = 0; j < 8; j++) {
+                                if (snapArray[j] == true) {
+                                    massList[j].transform.position = positionArray[numSnapped -1, count2];
+                                    massList[j].GetComponent<Rigidbody>().isKinematic = false;
+                                    count2 = count2 + 1;
+                                }
                             }
                         }
                     }
                     count = count + 1;
                 } else {
                     // check if mass is inside range
-                    if ((massList[i].transform.position.magnitude < 1.5)  && (massList[i].transform.parent == null || massList[i].transform.parent.gameObject.name != "Hand")) {
+                    if ((massList[i].transform.position.magnitude < 2)  && (massList[i].transform.parent == null || massList[i].transform.parent.gameObject.name != "Hand")) {
                         numSnapped = numSnapped + 1;
                         snapArray[i] = true;
                         massList[i].transform.SetParent(shell.transform);
                         int count2 = 0;
-                        for (int j = 0; j < 3; j++) {
-                            Debug.Log(numSnapped);
-                            Debug.Log(snapArray[j]);
-                            Debug.Log(j);
+                        for (int j = 0; j < 8; j++) {
                             if (snapArray[j] == true) {
                                 massList[j].transform.position = positionArray[numSnapped - 1, count2];
                                 massList[j].GetComponent<Rigidbody>().isKinematic = true;
