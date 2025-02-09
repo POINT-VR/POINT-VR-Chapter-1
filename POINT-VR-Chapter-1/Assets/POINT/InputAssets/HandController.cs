f159b0bca7d8b5fd3ca0a614b21590f4e8302b44
Dante Ratchev <104606374+RatchevD@users.noreply.github.com>
2025-02-09 15:25:13 -0600

    Updated HandController to Stop Opening/Closing Shell While Held
    Updated HandController to make sure the shell cannot be selected or unselected while its being held.

--- POINT-VR-Chapter-1/Assets/POINT/InputAssets/HandController.cs
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/HandController.cs
@@ -235,13 +235,12 @@ public class HandController : MonoBehaviour
                 hardwareController.VibrateHand();
             }
             laser.material.color = Color.magenta;
-            /////////////
-            SelectObject selectable = hit.collider.gameObject.GetComponent<SelectObject>();
-            if (selectable != null && selectable.GetSelected() == true)
-            {
-                laser.material.color = Color.green;
-            }
-            /////////////
+            // If the object has a Grab Mask but is a Selectable Object (ex. Density Shells)
+            // SelectObject selectable = hit.collider.gameObject.GetComponent<SelectObject>();
+            // if (selectable != null && selectable.GetSelected() == true)
+            // {
+            //     laser.material.color = Color.green;
+            // }
             if (grabbingTransform != null)
             {
                 lastGrabHit = grabbingTransform;
@@ -375,8 +374,9 @@ public class HandController : MonoBehaviour
         else if (Physics.Raycast(transform.position, transform.forward, out hit, 10f)) // Checking Selectable
         {
             SelectObject selectable = hit.collider.gameObject.GetComponent<SelectObject>();
-            if (selectable != null)
+            if (selectable != null && !hit.collider.gameObject.GetComponentInParent<HandController>())
             {
+                // Flipping value of Selectable Checkbox
                 selectable.SetSelected(!selectable.GetSelected());
             }
         }
