6a19fb07b036f96e9324a3d0e18132084b620bb2
Daniel Montoya <62759208+dkmontoya@users.noreply.github.com>
2025-02-21 23:17:12 -0600

    Implementation of Audio clips as objects
    Currently done for the narration audio in the tutorial. Adding tutorial audio script and tutorial audio manager script.

---
+++ POINT-VR-Chapter-1/Assets/TutorialAudioManager.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TutorialAudioManager : MonoBehaviour
+{
+    public static TutorialAudioManager Instance;
+
+    public TutorialAudio[] musicAudio, sfxAudio, voiceAudio;
+    public AudioSource musicSource, sfxSource, voiceSource;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    
+    // Tutorial I saw showed how to initialize the theme music
+    //private void Start()
+    //{
+    //    PlayMusic("MainTheme");
+    //}
+   
+    
+    
+    public void PlayMusic(string name)
+    {
+        TutorialAudio audio = Array.Find(musicAudio, sound => sound.name == name);
+        if (audio == null)
+        {
+            Debug.LogWarning("Music: " + name + " not found!");
+            return;
+        }
+        else
+        {
+            musicSource.clip = audio.clip;
+            musicSource.Play();
+        }
+        
+    }           
+    
+    public void PlaySFX(string name)
+    {
+        TutorialAudio audio = Array.Find(sfxAudio, sound => sound.name == name);
+        if (audio == null)
+        {
+            Debug.LogWarning("SFX: " + name + " not found!");
+            return;
+        }
+        else
+        {
+            sfxSource.clip = audio.clip;
+            sfxSource.Play();  
+        }
+        
+    }
+    
+    public void PlayVoice(string name)
+    {
+        TutorialAudio audio = Array.Find(voiceAudio, sound => sound.name == name);
+        if (audio == null)
+        {
+            Debug.LogWarning("Voice: " + name + " not found!");
+            return;
+        }
+        else
+        {
+            voiceSource.clip = audio.clip;
+            voiceSource.Play();
+
+        }
+        
+    }
+}
+
