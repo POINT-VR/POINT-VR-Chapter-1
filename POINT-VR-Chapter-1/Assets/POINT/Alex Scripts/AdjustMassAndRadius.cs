9e2e22248f79a0ed2a344b919a84153e26c469be
Alexander Lau <ajlau2@illinois.edu>
2025-07-17 18:20:01 -0500

    Initial Commit
    Bugs still present - switching branches, so saving this as a checkpoint.

---
+++ POINT-VR-Chapter-1/Assets/POINT/Alex Scripts/AdjustMassAndRadius.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdjustMassAndRadius : MonoBehaviour
+{
+    [SerializeField] private float initialMass;
+    [SerializeField] private float initialScale;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Rigidbody>().mass = initialMass;
+        transform.localScale = new Vector3(initialScale, initialScale, initialScale);
+    }
+
+    public void SetMass(float newMass)
+    {
+        gameObject.GetComponent<Rigidbody>().mass = newMass;
+    }
+
+    public void SetRadius(float newRadius)
+    {
+        transform.localScale = new Vector3(newRadius, newRadius, newRadius);
+    }
+}
