44f74781114bcdd726864f5f98f36499127c2cd8
Ethan Man <emanemail3919@gmail.com>
2026-02-01 00:36:26 -0600

    Added new scene reference transitions to all scenes

---
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/ChangeSceneScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using DevLocker.Utils;
+
+public class ChangeSceneScript : MonoBehaviour
+{
+    [Tooltip("Transition to this scene")]
+    [SerializeField] private SceneReference scene;
+    [SerializeField] GameObject player;
+
+    public void UIChangeScene()
+    {
+        //Ensure scene and player are set
+        if (scene != null && player != null) 
+        {
+            SceneManager.LoadScene(scene.SceneName);
+
+            //Solves issue where loading the scene puts the player in a pause state
+            if (player != null && player.TryGetComponent(out PauseController pauseController))
+            {
+                pauseController.Unpause();
+            }
+        }
+ 
+    }
+}
