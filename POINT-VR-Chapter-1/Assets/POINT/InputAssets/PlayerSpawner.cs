5712a28be41e7cb5923c7dbee516a455e827fb65
Mireille-T <mireille.tan@gmail.com>
2026-08-08 13:19:08 +0800

    Add rigidbody + collider to player base via script

--- POINT-VR-Chapter-1/Assets/POINT/InputAssets/PlayerSpawner.cs
+++ POINT-VR-Chapter-1/Assets/POINT/InputAssets/PlayerSpawner.cs
@@ -11,6 +11,13 @@ public class PlayerSpawner : MonoBehaviour
     private void Awake()
     {
         activePlayer = Instantiate(player, transform);
+
+        // Add rigidbody and collider to prevent player from going out of bounds
+        Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode.Continuous; // Smoother collision detection
+        rb.drag = Mathf.Infinity; // Max. drag to prevent sliding against boundary walls
+        this.gameObject.AddComponent<SphereCollider>();
     }
 
     public void UIChangeScene(int scene)
